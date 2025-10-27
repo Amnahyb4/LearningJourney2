12e4bf230b99a13d0d41956bb66e4fee185ec458
Amnah <amnahyb4@gmail.com>
2025-10-27 09:48:44 +0300

    Initial Commit

---
+++ LearningJourney2/LearningJourney2App.swift
@@ -0,0 +1,17 @@
+//
+//  LearningJourney2App.swift
+//  LearningJourney2
+//
+//  Created by Amnah Albrahim on 05/05/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningJourney2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
